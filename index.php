822e7203e307365664d5fdaadaf30410dd294b74
Yoyogi <yojinshi@hotmail.fr>
2011-11-19 14:48:05 +0100

    First commit

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+@set_time_limit(900); //15min.
+
+header('Content-type: text/html; charset=iso-8859-1');
+
+//remplace GET et POST par les noms de variables
+foreach ($_GET as $key => $value) $$key = $value;
+foreach ($_POST as $key => $value) $$key = $value;
+
 include_once "config.php";
+
+//initialisation des variables de navigation pour la premiere utilisation
+if(!isset($page))
+    $page = "";
 ?>
