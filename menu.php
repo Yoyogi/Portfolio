8899490c654b6bdd4527a00fbb608d3ca3d9dc62
Yoyogi <yojinshi@hotmail.fr>
2011-11-19 16:49:29 +0100

    change directory

--- menu.php
+++ menu.php
@@ -1,3 +1,11 @@
-<?php
-
-?>
+<div id="menu">
+    <div class="entete_menu">Menu de navigation</div>
+    <div id="element_menu">
+        <ul>
+            <li><a href="">Formation</a></li>
+            <li><a href="">Expériences professionnelles</a></li>
+            <li><a href="">Projets</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+</div>
